baaf5287767948b21f0978e90e3e0fd872ab87a4
Walkman295 <walkman295@yandex.ru>
2017-04-13 23:55:06 +0300

    Create comment-class (some manipulation & need data from DB)

---
+++ app/src/main/java/com/miet/walkman295/unirating/Comments.java
@@ -0,0 +1,31 @@
+package com.miet.walkman295.unirating;
+
+//действие - класс - ключевое слово для поиска
+
+public class Comments
+{
+    //обработчик нажатия кнопки - Section1_1_InfoUNI - "OnClickListener" (+ setOnClickListener)
+
+    //переход между фрагментами - Section1_1_InfoUNI - "ToChatListener"
+
+    //переход между Activity - Section1_1_InfoUNI - "ToMapListener"
+
+    //поиск элемента по ID во фрагменте - Section1_1_InfoUNI - "rootView.findViewById"
+
+    //заполнение списка данными - Section 1_1_InfoUNI - "ArrayAdapter"
+
+    //установка заголовка layout'a - Section 1_1_InfoUNI - "setTitle"
+
+    //связывание layout и фрагмента - Section 1_1_InfoUNI - "rootView"
+
+    //переход по ссылке при нажатии на кнопку - Section6_Donation - "webIntent"
+
+    /*
+    Заполнение БД:
+    1) Section1_SelectUNI - список всех ВУЗов в БД;
+    2) Section1_1_InfoUNI - рейтинг, название/город, адрес, телефон,
+    e-mail, сайт, координаты метки, информация, список преподавателей;
+    3) Отдельно от layout'ов: чаты, в которых состоит пользователь;
+    избранные ВУЗы пользователя;
+     */
+}
