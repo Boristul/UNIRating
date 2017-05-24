46684904e016cbacd0028d8af9e584a009ee7f9d
Walkman295 <walkman295@yandex.ru>
2017-05-24 23:12:44 +0300

    Added statistics activity (final)

--- app/src/main/java/com/miet/walkman295/unirating/Section1_1_1_Statistics.java
+++ app/src/main/java/com/miet/walkman295/unirating/Section1_1_1_Statistics.java
@@ -1,6 +1,7 @@
 package com.miet.walkman295.unirating;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.jjoe64.graphview.GraphView;
@@ -16,16 +17,34 @@ public class Section1_1_1_Statistics extends Activity {
         setContentView(R.layout.activity_statistics);
 
         GraphView graph = (GraphView) findViewById(R.id.graphics);
-        /*LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+
+        //график start
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
             new DataPoint(0,2),
             new DataPoint(1,6),
             new DataPoint(5,3)
-        });*/
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
+        });
+        //график end
+
+        //гистограмма start
+        /*BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
                 new DataPoint(0,2),
                 new DataPoint(1,6),
                 new DataPoint(5,3)
-        });
+        });*/
+        //гистограмма end
+
+        /*
+        установка цвета
+        толщина линии графика
+        отмечать точки на графике
+        радиус точек-отметок
+         */
+        series.setColor(Color.BLUE);
+        series.setThickness(10);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(15);
+
         graph.addSeries(series);
     }
 }
